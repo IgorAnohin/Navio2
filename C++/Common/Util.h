cb97d174e75cba8a4cf8ca52bbbe8e4af0896569
Igor Anohin <igor.anohin@emlid.com>
2017-10-06 19:59:46 +0300

    AHRS: delete realization from header and add imu in AHRS class, delete last global variable

--- C++/Common/Util.h
+++ C++/Common/Util.h
@@ -2,6 +2,7 @@
 
 #define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
 #define NAVIO2 3
+#define NAVIO 1
 
 int write_file(const char *path, const char *fmt, ...);
 int read_file(const char *path, const char *fmt, ...);
