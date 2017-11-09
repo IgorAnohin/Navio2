c75c2f9293c865d42ee79fc63ce71c90db02be3f
Igor Anokhin <igor.anohin@emlid.com>
2017-11-09 20:55:28 +0300

    python: ADC: add Navio+ entry

--- Python/RCInput.py
+++ Python/RCInput.py
@@ -3,16 +3,17 @@ import time
 import navio.Common.util
 
 import navio.Navio2.rcinput
+import navio.Navio.RCInput_Navio
 
 navio.Common.util.check_apm()
 
 if navio.Common.util.get_navio_version() == "NAVIO2":
 
     rcin = navio.Navio2.rcinput.RCInput()
+else:
+    rcin = navio.Navio.RCInput_Navio.RCInput_Navio()
 
     while (True):
-        period = rcin.read(2)
+        period = rcin.read(1)
         print period
         time.sleep(1)
-else:
-    print "No RCInput on Navio+"
