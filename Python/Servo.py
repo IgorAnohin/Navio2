fb09ffa80a8ad41cd8172fe8ff3d4c45bd614a8a
Igor Anokhin <igor.anohin@emlid.com>
2017-11-11 19:52:20 +0300

    python: Servo: add Navio+ support
    Add auto choice shield

--- Python/Servo.py
+++ Python/Servo.py
@@ -1,45 +1,25 @@
-import math
 import time
 
-
 import navio.Common.util
-import navio.Navio2.pwm
-from navio.Navio.adafruit_pwm_servo_driver import PWM
+import navio.Navio2.RCOutput_Navio2
+import navio.Navio.RCOutput_Navio
+
+PWM_OUTPUT = 0
+
+def get_pwm():
+	if (navio.Common.util.get_navio_version() == "NAVIO2"):
+		return navio.Navio2.RCOutput_Navio2.RCOutput_Navio2(PWM_OUTPUT)
+	else:
+		return navio.Navio.RCOutput_Navio.RCOutput_Navio(PWM_OUTPUT)
 
 navio.Common.util.check_apm()
 
-PWM_OUTPUT = 0
-SERVO_MIN = 1.250 #ms
-SERVO_MAX = 1.750 #ms
+with get_pwm() as pwm:
+	pwm.set_period(50)
+	pwm.enable()
 
-if (navio.Common.util.get_navio_version() == "NAVIO2"):
-
-	with navio.Navio2.pwm.PWM(PWM_OUTPUT) as pwm:
-		pwm.set_period(50)
-		pwm.enable()
-
-		while (True):
-			pwm.set_duty_cycle(SERVO_MIN)
-			time.sleep(1)
-			pwm.set_duty_cycle(SERVO_MAX)
-			time.sleep(1)
-
-else:
-
-	#drive Output Enable in PCA low
-	#pin = navio.Navio.gpio.Pin(27)
-	#pin.write(0)
-
-	frequency = 50
-
-	NAVIO_RCOUTPUT_1 = 3
-
-	#convert mS to 0-4096 scale:
-	SERVO_MIN_ms = math.trunc((SERVO_MIN * 4096.0) / (1000.0 / frequency) - 1)
-	SERVO_MAX_ms = math.trunc((SERVO_MAX * 4096.0) / (1000.0 / frequency) - 1)
-
-	pwm = PWM(0x40, debug=False)
-
-	# Set frequency to 60 Hz
-	pwm.setPWMFreq(frequency)
-
+	while (True):
+		pwm.set_duty_cycle(pwm.SERVO_MIN)
+		time.sleep(1)
+		pwm.set_duty_cycle(pwm.SERVO_MAX)
+		time.sleep(1)
